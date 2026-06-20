b898f111bce8228b800376808b60a2a3f4a407a0
Marina Makarova <mkrvmrn.00@gmail.com>
2026-06-20 19:28:27 -0300

    404

--- views/404.php
+++ views/404.php
@@ -1,2 +1,11 @@
-<img src="img/404.png" alt="Disco roto" class="d-block mx-auto m-5 img-404">
-<h1 class="text-center m-5">404: Página no encontrada</h1>
+<section class="error-page">
+    <img src="img/404.png" alt="Disco roto" class="img-404">
+
+    <h1>404: Página no encontrada</h1>
+
+    <p>No pudimos encontrar la página solicitada.</p>
+
+    <a href="index.php?sec=home" class="error-link">
+        Volver al inicio
+    </a>
+</section>
